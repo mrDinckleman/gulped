5d58f433b6fceb38362a411086b2ba2476531ac6
Sergey Kovalchuk <admin@vitam.in.ua>
2018-01-17 20:38:21 +0300

    Add .editorconfig Add views (gulp-ejs, gulp-data) Fix indents

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    sourceMaps = require('gulp-sourcemaps');
+  ejs = require('gulp-ejs'),
+  data = require('gulp-data'),
+  sass = require('gulp-sass'),
+  sourceMaps = require('gulp-sourcemaps'),
+  path = require('path'),
+  fs = require('fs');
+
+gulp.task('ejs', function () {
+  return gulp.src('app/views/*.ejs')
+    .pipe(data(function (file) {
+      return JSON.parse(fs.readFileSync(file.path.substr(0, file.path.indexOf(file.extname)) + '.json'));
+    }))
+    .pipe(ejs({}, {}, { ext: '.html' }))
+    .pipe(gulp.dest('public'));
+});
 
 gulp.task('sass', function () {
-    return gulp.src('app/styles/*.scss')
-        .pipe(sourceMaps.init())
-            .pipe(sass().on('error', sass.logError))
-        .pipe(sourceMaps.write('./'))
-        .pipe(gulp.dest('public/assets/css'));
+  return gulp.src('app/styles/*.scss')
+    .pipe(sourceMaps.init())
+      .pipe(sass().on('error', sass.logError))
+    .pipe(sourceMaps.write('./'))
+    .pipe(gulp.dest('public/assets/css'));
 });
 
 gulp.task('default', gulp.series('sass'));
